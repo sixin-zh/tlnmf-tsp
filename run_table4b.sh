4e6260a7960d963ef087a19fde8f15990718a8a9
Sixin Zhang <sixin.zhang@irit.fr>
2022-06-28 09:12:05 +0200

    clean run.sh

--- run_table4b.sh
+++ run_table4b.sh
@@ -12,11 +12,6 @@ export NUMEXPR_NUM_THREADS=8
 python jointdiag/tlnmf2b_sci_batch.py  -sn nonstationary440_sim1_5k -tn nonstationary440_sim1_5k -epsnmf 5e-7 -runid 4 -s 100 -itl 100 -pertl 1 -pernmf 10 -nmfme 1
 python jointdiag/tlnmfJD2b_sci_batch.py  -sn nonstationary440_sim1_5k -s 100 -ratio 10 -itl 100  -epsc 5e-7 -runid 4 -nmfme 1 
 
-# FORGET
-# SKIP to get compu. time with T=20
-#python jointdiag/tlnmf2_sci_batch.py  -epsnmf 5e-7 -s 100 -itl ?? -pertl 1 -pernmf 10 -runid 8
-#
-
 ## Case R=10
 #python jointdiag/tlnmf2_best_sci_batch.py  -epsnmf 5e-8 -s 100 -itl 500 -pertl 1 -pernmf 10 -nbruns 10
 #python jointdiag/tlnmf2b_sci_batch.py  -epsnmf 5e-8 -runid 4 -s 100 -itl 500 -pertl 1 -pernmf 10
@@ -25,6 +20,4 @@ python jointdiag/tlnmfJD2b_sci_batch.py  -sn nonstationary440_sim1_5k -s 100 -ra
 
 #python jointdiag/tlnmf2_sci_batch.py  -epsnmf 5e-8 -s 100 -itl 20 -pertl 1 -pernmf 10 -runid 4 -cache 0
 
-##CASE R=1, FORGET python jointdiag/tlnmf2_best_sci_batch.py  -epsnmf 5e-8 -s 100 -itl 500 -pertl 1 -pernmf 1 -nbruns 10
 
-#python jointdiag/tlnmfJD2_sci_batch.py  -s 100 -itl ?? -inmf 200 -epsc 5e-8 -runid 8
